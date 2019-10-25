3dd36b2b95da88543ed77ab5c036a34363fd6b53
unknown <edpaez54g5@gmail.com>
2019-10-24 20:01:40 -0400

    apologies on the dellay

---
+++ module-1/lab-api-scavenger-game/your-code/importantstuff.py
@@ -0,0 +1,16 @@
+usename    = ""
+psw        = ""
+
+git_user = 'eduarolpaez'
+git_token = '634c2462a56278289bf230c4b130ac2e2aada144'
+
+access_token = '1135979892301017089-5aTrKwu2wUj9xOYIjvTtE3ETEz3CIl'
+access_token_secret = 'FwMUqjpVcoIOVLBzrdEx7b8kjSZ0JLFxAhmdgKO5SnGQN'
+
+consumer_key = 'H8Y4Fu6DAhaIaXEjZZIcxCNdK'
+consumer_secret = 'rQxgheDNoMFToNB2DeULFakx53GIRGlW9mYVkH7nxRqZW52BWq'
+
+
+server_mysql_username = 'root'
+server_mysql_password = 'Saryn Code54'
+
